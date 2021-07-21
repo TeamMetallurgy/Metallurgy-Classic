3ea416b54cef065d46e9523ce6006966cc0dd5c1
Mitchell Manning <RebelKeithy@gmail.com>
2021-07-21 16:45:40 -0500

    Added utility items match, igniter, fertilizer and tar

--- src/main/java/com/teammetallurgy/metallurgyclassic/MetallurgyClassic.java
+++ src/main/java/com/teammetallurgy/metallurgyclassic/MetallurgyClassic.java
@@ -1,5 +1,6 @@
 package com.teammetallurgy.metallurgyclassic;
 
+import com.teammetallurgy.metallurgyclassic.items.IgniterItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
@@ -12,6 +13,7 @@ import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Pair;
 import net.minecraft.util.registry.Registry;
+import org.lwjgl.system.CallbackI;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -27,5 +29,14 @@ public class MetallurgyClassic implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		MetalRegistry.instance().parseCSV("/metals_data.csv");
+
+		var magnesium_igniter = new IgniterItem(new FabricItemSettings().maxDamage(256).group(ItemGroup.TOOLS));
+		var match = new IgniterItem(new FabricItemSettings().maxDamage(1).group(ItemGroup.TOOLS));
+		var tar = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+		var fertilizer = new BoneMealItem(new FabricItemSettings().group(ItemGroup.MISC));
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "match"), match);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "magnesium_igniter"), magnesium_igniter);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "tar"), tar);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "fertilizer"), fertilizer);
 	}
 }
---
+++ src/main/java/com/teammetallurgy/metallurgyclassic/items/IgniterItem.java
@@ -0,0 +1,9 @@
+package com.teammetallurgy.metallurgyclassic.items;
+
+import net.minecraft.item.FlintAndSteelItem;
+
+public class IgniterItem extends FlintAndSteelItem {
+    public IgniterItem(Settings settings) {
+        super(settings);
+    }
+}
